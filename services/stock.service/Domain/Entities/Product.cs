9e6a8932b636a0a3b77d993e73c6565c5b2e701f
LucasSousa <lucassousasertania@gmail.com>
2025-08-12 19:38:03 -0300

    feat: add Product class with properties and methods

---
+++ services/stock.service/Domain/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities.Common;
+
+namespace Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+
+        public string Name { get; private set; } = default!;
+        public string Description { get; private set; } = default!;
+        public int Price { get; private set; }
+        public int InStock { get; private set; }
+        public string Sku { get; private set; } = default!;
+        public string Slug { get; private set; } = default!;
+
+        public Product(string name, string description, int price, int inStock, string sku, string slug)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            InStock = inStock;
+            Sku = sku;
+            Slug = slug;
+        }
+
+        public void UpdateProductData(string name, string description, int price, int inStock, string sku, string slug)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            InStock = inStock;
+            Sku = sku;
+            Slug = slug;
+        }
+    }
+}
