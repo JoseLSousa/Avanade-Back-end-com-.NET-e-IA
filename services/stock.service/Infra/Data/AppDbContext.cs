da65069c0fd3d43c158e82f2c752289eafc3eaf1
LucasSousa <lucassousasertania@gmail.com>
2025-08-12 19:39:40 -0300

    feat: add AppDbContext and repository implementations

---
+++ services/stock.service/Infra/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> context) : DbContext(context)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ services/stock.service/Infra/Repositories/UnitOfWorkImp.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Interfaces;
+using Infra.Data;
+
+namespace Infra.Repositories
+{
+    public class UnitOfWorkImp(AppDbContext context) : UnitOfWork
+    {
+        public async Task Commit(CancellationToken concellationToken)
+        {
+            await context.SaveChangesAsync(concellationToken);
+        }
+    }
+}
