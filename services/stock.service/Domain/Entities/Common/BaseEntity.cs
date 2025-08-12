1e7399786340dbc95b2f452312823e5dc9838e78
LucasSousa <lucassousasertania@gmail.com>
2025-08-12 19:37:47 -0300

    feat: add BaseEntity class with default properties

---
+++ services/stock.service/Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; private set; } = DateTime.UtcNow;
+
+        public void Update() => UpdatedAt = DateTime.UtcNow;
+    }
+}
